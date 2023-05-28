83bb3ee089e088dff769af64033b62c95282d19c
Shubham D <45649650+shubhamd99@users.noreply.github.com>
2023-05-28 12:27:07 +0530

    react native init

---
+++ AbsoluteImports/App.tsx
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet, View} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  const backgroundStyle = {
+    backgroundColor: Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={[styles.safeAreaView, backgroundStyle]}>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <View style={styles.container}></View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  safeAreaView: {
+    flex: 1,
+  },
+});
+
+export default App;
