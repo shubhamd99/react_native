51478571bdd10d21a75b06b65f69a8868784dfa1
Shubham D <45649650+shubhamd99@users.noreply.github.com>
2024-05-25 23:19:39 +0530

    add podcast screen

--- mypodcast/src/navigators/MainTabNavigators.tsx
+++ mypodcast/src/navigators/MainTabNavigators.tsx
@@ -5,6 +5,8 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ListenNowScreen from '../components/listenNow/ListenNowScreen';
 import SearchScreen from '../components/search/SearchScreen';
 import LibraryScreen from '../components/library/LIbraryScreen';
+import PodcastDetailsScreen from '../components/podcastDetails/PodcastDetailsScreen';
+import {theme} from '../constants/theme';
 
 const ListenNowStack = createNativeStackNavigator();
 
@@ -14,8 +16,9 @@ const ListenNowStackNavigator = () => {
       <ListenNowStack.Screen
         options={{
           title: 'Listen Now',
+          headerShown: false,
         }}
-        name="ListenNowStack"
+        name="ListenNow"
         component={ListenNowScreen}
       />
     </ListenNowStack.Navigator>
@@ -26,8 +29,17 @@ const SearchStack = createNativeStackNavigator();
 
 const SearchStackNavigator = () => {
   return (
-    <SearchStack.Navigator>
-      <SearchStack.Screen name="SearchStack" component={SearchScreen} />
+    <SearchStack.Navigator
+      screenOptions={{
+        headerTintColor: theme.color.blueLight,
+        headerShown: false,
+      }}>
+      <SearchStack.Screen name="Search" component={SearchScreen} />
+      <SearchStack.Screen
+        name="PodcastDetails"
+        component={PodcastDetailsScreen}
+        options={{headerTitle: ''}}
+      />
     </SearchStack.Navigator>
   );
 };
@@ -36,8 +48,11 @@ const LibraryStack = createNativeStackNavigator();
 
 const LibraryStackNavigator = () => {
   return (
-    <LibraryStack.Navigator>
-      <LibraryStack.Screen name="LibraryStack" component={LibraryScreen} />
+    <LibraryStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <LibraryStack.Screen name="Library" component={LibraryScreen} />
     </LibraryStack.Navigator>
   );
 };
@@ -46,14 +61,12 @@ const MainTab = createBottomTabNavigator();
 
 const MainTabNavigator = () => {
   return (
-    <MainTab.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}>
+    <MainTab.Navigator>
       <MainTab.Screen
         name="ListenNow"
         options={{
           title: 'Listen Now',
+          headerShown: false,
         }}
         component={ListenNowStackNavigator}
       />
